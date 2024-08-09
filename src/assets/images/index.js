b3c605db0527b9d723dec3b569341a3d02d60ce8
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-08-09 06:07:26 +0100

    feat::hero-section Complete

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -7,6 +7,10 @@ import line1 from "./Line 1.png"
 import line2 from "./Line 2.png"
 import apple from "./apple.png"
 import google from "./google.png"
+import hero1 from "./hero1.png"
+import hero2 from "./hero2.png"
+import hero3 from "./hero3.png"
+import plane from "./plane.png"
 
 
-export {logo, lady, man, lady1, stars, line1, line2, apple, google}
+export {logo, lady, man, lady1, stars, line1, line2, apple, google, hero1, hero2, hero3, plane}
