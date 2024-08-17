54d0a3cf81e7e80f1832865f7705bf33738df546
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-08-17 21:30:06 +0100

    feat::booking form UI

---
+++ src/assets/images/lodging/index.js
@@ -0,0 +1,10 @@
+import Bengys from './Bengys.png'
+import Bestwestern from './bestWestern.png'
+import carltonswiss from './Carltonswiss.png'
+import ekohotel from './ekoHotel.png'
+import entirevilla from './entireVilla.png'
+import lugard from './Lugard.png'
+import luxe from './Luxe.png'
+import transcorp from './Transcorp.png'
+
+export {Bengys, Bestwestern, carltonswiss, ekohotel, entirevilla, lugard, luxe, transcorp}
--- src/pages/discoverpage/lodging/index.jsx
+++ src/pages/discoverpage/lodging/index.jsx
@@ -1,11 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SearchBar } from "../../../components/searchbar"
 import design from "./design.module.css"
 import { UserContext } from "../../../context/appContext";
 import { FadeLoader } from "react-spinners";
+import { airBnB, Apartments, bathhub, bedAndBreakfast, Hotels, panicButton, Resorts, smartHome, surveillance, tv, Villas, waves, wifi } from "../../../assets";
+import { items } from "./data";
 
 export const Lodging = () => {
   const {loading} = useContext(UserContext);
+  const [count, setCount] = useState (1)
+
+  const decrement = () => {
+    setCount (count - 1)
+  }
+
+  const increment = () => {
+    setCount (count + 1)
+  }
 
   return (
     <div className={design.container}>
@@ -32,13 +43,111 @@ export const Lodging = () => {
 
         <div className={design.bookingSection}>       
           <form className={design.bookingForm}>
+
+            {/* Price Range */}
             <div className={design.priceRange}>
-              <input type="text" className={design.priceInput} />
-              <input type="text" className={design.priceInput} />
+              <p>Price</p>
+              <div>
+                <input type="text" placeholder="From"/>
+                <input type="text" placeholder="To"/>
+              </div>
+            </div>
+
+            {/* Property Type */}
+
+            <div className={design.propertyType}>
+              <p>Property Type</p>
+              <div className={design["proptypelist"]}>
+                <div>
+                  <img src={bedAndBreakfast} alt="bNb" />
+                  <p>Bed and Breakfast</p>
+                </div>
+                <div>
+                  <img src={Apartments} alt="Apartments" />
+                  <p>Apartments</p>
+                </div>
+                <div>
+                  <img src={airBnB} alt="Air BNB" />
+                  <p>Airbnb</p>
+                </div>
+                <div>
+                  <img src={Villas} alt="Villas" />
+                  <p>Villas</p>
+                </div>
+                <div>
+                  <img src={Hotels} alt="Hotels" />
+                  <p>Hotels</p>
+                </div>
+                <div>
+                  <img src={Resorts} alt="Resorts" />
+                  <p>Resorts</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Number of guests */}
+            <div className={design.numOfGuests}>
+              <p>Number of Guests</p>
+              <div>
+                <button onClick={decrement}>--</button>
+                <p>{count}</p>
+                <button onClick={increment}>+</button>
+              </div>
+            </div>
+
+            {/* Convenience */}
+
+            <div className={design.convenience}>
+              <p>Property Type</p>
+              <div className={design["convenienceList"]}>
+                <div>
+                  <img src={waves} alt="bNb" />
+                  <p>Beach View</p>
+                </div>
+                <div>
+                  <img src={tv} alt="Apartments" />
+                  <p>TV</p>
+                </div>
+                <div>
+                  <img src={wifi} alt="Wifi" />
+                  <p>Wi-Fi</p>
+                </div>
+                <div>
+                  <img src={panicButton} alt="Panic Button" />
+                  <p>Panic Button</p>
+                </div>
+                <div>
+                  <img src={smartHome} alt="Smart Home" />
+                  <p>Smart Home</p>
+                </div>
+                <div>
+                  <img src={surveillance} alt="Surveillance Features" />
+                  <p>Surveillance</p>
+                </div>
+                <div>
+                  <img src={bathhub} alt="Outdoor Bathhub" />
+                  <p>Outdoor Baths</p>
+                </div>
+              </div>
+            </div>
+
+            <div className={design.submitBtn}>
+              <button>Apply</button>
             </div>
           </form>
 
-          <section className={design.bookingReview}>Right Section</section>
+          <section className={design.bookingReview}>
+            {items.map(item => (
+              <div key={item.id} className={design.hotelCard}>
+                <img src={item.backgroundImg} alt="" />
+                <p className={design.titleField}>{item.title}</p>
+                <p className={design.location}>{item.location}</p>
+                <p className={design.review}>{item.rating}</p>
+                <p className={design.status}>{item.status}</p>
+                <p className={design.price}>{item.price}</p>
+              </div>
+            ))}
+          </section>
         </div>
       </div>
     </div>
