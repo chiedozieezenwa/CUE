e83edeb47166d4d7555ba2baff739f27d0b6611d
chukwuebuka <emmanuel.henry71@gmail.com>
2024-08-19 15:55:11 +0100

    feat:: added few features in rental page

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,12 @@
-import closeIcon from './closeIcon.png'
-import hidePassword from './hidePassword.png'
-import showPassword from './showPassword.png'
-import searcharrow from './searcharrow.png'
+import closeIcon from './closeIcon.png';
+import hidePassword from './hidePassword.png';
+import showPassword from './showPassword.png';
+import searcharrow from './searcharrow.png';
+import icon1 from "./Icon1.png";
+import icon2 from "./Icon2.png";
+import icon3 from "./icon3.png";
+import icon11 from "./icon11.png";
+import icon22 from "./icon22.png";
+import icon33 from "./icon33.png";
 
-export {closeIcon, hidePassword, showPassword, searcharrow}
+export {closeIcon, hidePassword, showPassword, searcharrow, icon1, icon2, icon3, icon11, icon22, icon33}
