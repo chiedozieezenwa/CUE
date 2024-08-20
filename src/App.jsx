111ce31c6dded7ca2663f3c94e4293b9a4b21719
soloTech <uchesolomon61@gmail.com>
2024-08-20 16:45:27 +0100

    fetched the hotel succesfully from the api

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import router from './appRouter'
 import { UserProvider } from './context/appContext'
+import { LodgingProvider } from './context/LodgingContext'
+import { SearchProvider } from './context/searchContext'
 
  const App = () => {
     return (
       <UserProvider>
+        <LodgingProvider>
+          <SearchProvider>
         <RouterProvider router={router} />
+        </SearchProvider>
+        </LodgingProvider>
+        
       </UserProvider>
     );
  }
---
+++ src/context/LodgingContext.jsx
@@ -0,0 +1,71 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const LodgingContext = createContext();
+
+export const LodgingProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(1);
+  const [hotels, setHotels] = useState([]);
+  const [filters, setFilters] = useState({
+    minPrice: "",
+    maxPrice: "",
+    type: "",
+    brand_new: "",
+    wifi: "",
+    smarthome: "",
+    surveillance: "",
+    panic_button: ""
+  });
+
+  const fetchHotels = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("https://cue-api-3tyr.onrender.com/api/v1/hotels", {
+        params: filters
+      });
+      setHotels(response.data.data.hotels);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching hotels:", error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchHotels();
+  }, []);
+
+  const handleFilterChange = (e) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => Math.max(prevCount - 1, 1));
+  };
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <LodgingContext.Provider
+      value={{
+        loading,
+        count,
+        hotels,
+        filters,
+        setFilters,
+        fetchHotels,
+        handleFilterChange,
+        decrement,
+        increment,
+      }}
+    >
+      {children}
+    </LodgingContext.Provider>
+  );
+};
